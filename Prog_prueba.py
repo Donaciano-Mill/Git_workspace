93e8fc445e8e73b7bd6e34a45364cbf74cfc9d35
Donaciano Millan <dmillan1295@outlook.com>
2022-03-02 23:35:21 -0600

    Primer programa en git

--- Prog_prueba.py
+++ Prog_prueba.py
@@ -1,2 +1,11 @@
 for i in range(0,5):
     print("Hola a tos")
+
+Print("Final del codigo")
+ 
+ def suma(num1,num2):
+   resultado =num1+num2
+     return resultado
+
+if __name__==__main__:
+    suma()
